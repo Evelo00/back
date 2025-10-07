a9e716f135ea13b2add83ed051f66a305827a770
Eyver Vergara <eveloxD@gmail.com>
2025-10-06 19:10:10 -0500

    .

--- src/controllers/vitrinaCounter.controller.ts
+++ src/controllers/vitrinaCounter.controller.ts
@@ -1,6 +1,6 @@
 // src/controllers/vitrina.controller.ts
 import type { Request, Response } from "express";
-import VitrinaCounter from "../models/vitrinaCounter";
+import VitrinaCounter from "../models/vitrinaCounter.js";
 
 export const getVitrinas = async (req: Request, res: Response) => {
   try {
