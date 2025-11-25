7619f69cb8bdba85f9ca6fc0fe26683eac971067
Eyver Vergara <eveloxd@gmail.com>
2025-11-25 03:25:06 -0500

    server

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,19 +1,17 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sequelize = exports.connectDB = void 0;
 const sequelize_1 = require("sequelize");
-const dotenv_1 = __importDefault(require("dotenv"));
-if (process.env.NODE_ENV !== "production") {
-    dotenv_1.default.config();
-}
+// import dotenv from "dotenv";
+// if (process.env.NODE_ENV !== "production") {
+//   dotenv.config();
+// }
 const dbHost = process.env.DB_HOST;
 const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
-const dbPort = Number(process.env.DB_PORT || 11550);
+const dbPort = Number(process.env.DB_PORT || 5432);
+console.log("DB CONFIG ->", { dbHost, dbPort, dbUser, dbName });
 if (!dbHost)
     throw new Error("DB_HOST no está definido");
 if (!dbName)
